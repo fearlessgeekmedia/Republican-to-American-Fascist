f79f4dc7a86d785f881ad6a8ace6bac0319635fc
Fearless Geek Media <114188406+fearlessgeekmedia@users.noreply.github.com>
2026-02-11 17:53:01 -0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,34 @@
+function replaceText(node) {
+  const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null, false);
+  let textNode;
+  while ((textNode = walker.nextNode())) {
+    textNode.nodeValue = textNode.nodeValue
+      .replace(/\bRepublicans\b/g, 'American Fascists')
+      .replace(/\bRepublican\b/g, 'American Fascist')
+      .replace(/\brepublicans\b/g, 'american fascists')
+      .replace(/\brepublican\b/g, 'american fascist');
+  }
+}
+
+replaceText(document.body);
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        replaceText(node);
+      } else if (node.nodeType === Node.TEXT_NODE) {
+        node.nodeValue = node.nodeValue
+          .replace(/\bRepublicans\b/g, 'American Fascists')
+          .replace(/\bRepublican\b/g, 'American Fascist')
+          .replace(/\brepublicans\b/g, 'american fascists')
+          .replace(/\brepublican\b/g, 'american fascist');
+      }
+    });
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
